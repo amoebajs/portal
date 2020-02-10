61fb98d5890266211bb466a3ed2b478d9f6e153a
bigmogician <sunaoran@youzan.com>
2020-02-10 12:10:41 +0800

    feat: use new docker build rules

--- scripts/docker.js
+++ scripts/docker.js
@@ -1,20 +1,41 @@
 const path = require("path");
-const { spawn, spawnSync } = require("child_process");
+const fs = require("fs");
+const { spawnSync } = require("child_process");
+const dockerJson = require("../docker");
 
 const args = process.argv.slice(2);
 
 const yarn = /^win/.test(process.platform) ? "yarn.cmd" : "yarn";
 
-let version = `0.0.${new Date().getTime()}`;
+const lastVersion = dockerJson.version || "0.0.1";
+const dockerName = dockerJson.name || "demo";
+
+let version = updateVersion(lastVersion);
 let quick = false;
 for (const argv of args) {
   if (argv.startsWith("-v=") || argv.startsWith("--version=")) {
-    version = argv.split("=")[1];
+    version = updateVersion(lastVersion, argv.split("=")[1]);
     continue;
   }
   if (argv.startsWith("-q=") || argv.startsWith("--quick=")) {
     quick = argv.split("=")[1] === "true";
     continue;
+  }
+}
+
+function updateVersion(oldV, newV) {
+  const isPreVersion = (newV || oldV).indexOf("-") > 0;
+  if (isPreVersion) {
+    let [p, r] = (newV || oldV).split("-");
+    let [rp, rv] = r.split(".");
+    let nrv = Number(rv || 0);
+    nrv += 1;
+    return [p, [rp, nrv].join(".")].join("-");
+  } else {
+    let [a, b, c] = (newV || oldV).split(".");
+    let nc = Number(c);
+    if (!newV) nc += 1;
+    return [a, b, nc].join(".");
   }
 }
 
@@ -33,12 +54,17 @@ if (!quick) {
 }
 
 const command = "docker";
-const params = ["build", "-t", `bigmogician/ews-core:${version}`, "."];
+const params = ["build", "-t", `${dockerName}:${version}`, "."];
 
 console.log("$ " + command + " " + params.join(" "));
 
-spawn("docker", ["build", "-t", `bigmogician/ews-core:${version}`, "."], {
-  env: process.env,
-  cwd: path.resolve(__dirname, ".."),
-  stdio: ["pipe", process.stdout, process.stderr],
-});
+runSync(command, params);
+
+console.log("Update Docker Configs --> ");
+
+const dockerJsonPath = path.resolve(__dirname, "..", "docker.json");
+const newDockerJsonString = JSON.stringify({ ...dockerJson, version }, null, "  ");
+
+fs.writeFileSync(dockerJsonPath, newDockerJsonString, { flag: "w+", encoding: "utf8" });
+
+console.log(newDockerJsonString);
